e08ea185074891cd0144be9893128f2920b7e56b
jinqinghua <qinghuajin@cmchina.com>
2017-04-11 01:50:29 +0800

    delete unused code in SequenceGeneratorTest.java

--- src/test/java/yummy/util/SequenceGeneratorTest.java
+++ src/test/java/yummy/util/SequenceGeneratorTest.java
@@ -1,6 +1,5 @@
 package yummy.util;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,61 +17,61 @@ import java.util.concurrent.CountDownLatch;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath*:spring/**/*.xml"})
 @TestPropertySource(properties = {
-		"sequence.work.id.store.driver=com.mysql.jdbc.Driver",
-		"sequence.work.id.store=jdbc:h2:mem:dataSource",
-		"sequence.work.id.store.user=sa",
-		"sequence.work.id.store.password="
+        "sequence.work.id.store.driver=com.mysql.jdbc.Driver",
+        "sequence.work.id.store=jdbc:h2:mem:dataSource",
+        "sequence.work.id.store.user=sa",
+        "sequence.work.id.store.password="
 })
 public class SequenceGeneratorTest {
-	@Autowired
-	private SnowflakeSequenceGenerator sequenceGenerator;
+    @Autowired
+    private SnowflakeSequenceGenerator sequenceGenerator;
 
-	@Test
-	public void test() {
-		final int threadCount = 100;
-		final int idCount = 10000;
-		final Set<Long> set = new ConcurrentSkipListSet<>();
-		final CountDownLatch startAwait = new CountDownLatch(threadCount);
-		final CountDownLatch endAwait = new CountDownLatch(threadCount);
+    @Test
+    public void test() {
+        final int threadCount = 100;
+        final int idCount = 10000;
+        final Set<Long> set = new ConcurrentSkipListSet<>();
+        final CountDownLatch startAwait = new CountDownLatch(threadCount);
+        final CountDownLatch endAwait = new CountDownLatch(threadCount);
 
-		Thread[] threads = new Thread[threadCount];
-		for (int i = 0; i < threadCount; i++) {
-			threads[i] = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						startAwait.await();
-						for (int i = 0; i < idCount; i++) {
-							long id = sequenceGenerator.nextId();
-							set.add(id);
-						}
-					} catch (Exception e) {
-						System.out.println(e.getMessage());
-					}
-					endAwait.countDown();
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        startAwait.await();
+                        for (int i = 0; i < idCount; i++) {
+                            long id = sequenceGenerator.nextId();
+                            set.add(id);
+                        }
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    endAwait.countDown();
 
-				}
-			});
-		}
+                }
+            });
+        }
 
-		for (int k = 0; k < threadCount; k++) {
-			threads[k].start();
-			startAwait.countDown();
-		}
+        for (int k = 0; k < threadCount; k++) {
+            threads[k].start();
+            startAwait.countDown();
+        }
 
-		try {
-			endAwait.await();
-		} catch (InterruptedException e) {
-		}
+        try {
+            endAwait.await();
+        } catch (InterruptedException e) {
+        }
 
-		System.out.println(set.size());
-		Long l = set.iterator().next();
-		System.out.println(l);
-		System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.formattedDateString(l));
-		System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.sequence(l));
-		System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.timeStamp(l));
-		System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.workId(l));
+        System.out.println(set.size());
+        Long l = set.iterator().next();
+        System.out.println(l);
+        System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.formattedDateString(l));
+        System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.sequence(l));
+        System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.timeStamp(l));
+        System.out.println(SnowflakeSequenceGenerator.SequenceDecoder.workId(l));
 
-		assert set.size() == threadCount * idCount;
-	}
+        assert set.size() == threadCount * idCount;
+    }
 }
